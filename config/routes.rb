591421de47a478415cd5d0e54c9019aa5672bbd1
Michaela Sivertsen <mjsivertsen@msn.com>
2021-10-08 12:55:28 -0600

    empty components and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
-  namespace :api do
-    post "/memes", to: "memes#create"
-    post "/memes1", to: "memes#create1"
-    get "/users", to: "users#index"
-    get "/users/:id", to: "users#show"
-    put "/users/:id", to: "users#update"
+  # namespace :api do
+  #   post "/memes", to: "memes#create"
+  #   post "/memes1", to: "memes#create1"
+  #   get "/users", to: "users#index"
+  #   get "/users/:id", to: "users#show"
+  #   put "/users/:id", to: "users#update"
+  #   resources :things
+  # end
 
-    resources :things
-  end
+  namespace :api do
+    resources :users do
+      resources :houses do
+        resources :visits
+      end
+    end
+    resources :users do
+      resources :costumes do
+        resources :visits
+      end
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
