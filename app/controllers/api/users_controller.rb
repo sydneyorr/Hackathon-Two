c3598574783351e678897d8ce6863609f1981e85
Alan Mamulski <alanmamulski@gmail.com>
2021-10-09 10:08:13 -0600

    User and costume controllers

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,46 @@
 class Api::UsersController < ApplicationController
+  # before_action :authenticate_user!
+  before_action :set_user, only: [:show, :destroy, :update]
+    
+    def index
+      render json: User.all
+    end
+    
+    def show
+      render json: @userDeviseTokenAuth::RegistrationsController
+    end
+    
+    def create
+      @user = User.new(user_params)
+      if (@user.save)
+        render json: @user
+      else
+        render json: {error: @user.errors}, status: 422
+      end
+    end
+    
+    def update
+      if(@user.update(user_params))
+        render json: @user
+      else
+        render json: {error: @user.errors}, status: 422
+      end
+    end
+    
+    def destroy
+      @user.destroy
+      render json: @user
+    end
+    
+    
+    private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :house, :costume )
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
 end
